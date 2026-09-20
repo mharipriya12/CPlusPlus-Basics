eaad5235c69b25ba2e3579c92062409cee7ae810
mharipriya12 <haripriyamuthukrishnansvg345@gmail.com>
2026-09-20 14:36:19 +0530

    Implement reverse number program in C++

---
+++ reverse_number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, reverse = 0;
+
+    cin >> n;
+
+    while (n != 0) {
+        reverse = reverse * 10 + n % 10;
+        n /= 10;
+    }
+
+    cout << "Reverse = " << reverse;
+
+    return 0;
+}
